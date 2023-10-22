5e97e5c7dca2fb4d941941c74be646145c6ddaef
Midhunsrisai0 <104824866+Midhunsrisai0@users.noreply.github.com>
2023-10-22 19:16:35 +0530

    Time: 8 ms (76.16%) | Memory: 43.7 MB (31.63%) - LeetSync

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+      LinkedHashMap<Integer,Integer> hm = new LinkedHashMap<>();
+      for(int i=0;i<nums.length;i++){
+          int n = nums[i];
+          int c=0;
+          for(int j=0;j<nums.length;j++){
+              if(nums[j]<n) c++;
+          }
+          hm.put(n,c);
+      }  
+      int b []=new int[nums.length];
+      for(int i=0;i<nums.length;i++){
+          b[i] = hm.get(nums[i]);
+      } 
+      return b;
+    }
+}
