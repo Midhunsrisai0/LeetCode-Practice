f34cff7a31133cbccb6fcaa2a599a15ec123ccc2
Midhunsrisai0 <104824866+Midhunsrisai0@users.noreply.github.com>
2023-10-27 21:30:37 +0530

    Time: 7 ms (56.15%) | Memory: 43.9 MB (87.99%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,14 +1,14 @@
 class Solution {
     public String reverseWords(String inp) {
-        String r ="";
+        StringBuilder r =new StringBuilder();
         String[] srr = inp.split(" ");
         for(int i=0;i<srr.length;i++){
             String s = srr[i];
             for(int j=s.length()-1;j>=0;j--){
-                r+=s.charAt(j);
+                r.append(s.charAt(j));
             }
-            if(i<srr.length-1) r+=" ";
+            if(i<srr.length-1) r.append(" ");
         }
-        return r;
+        return r.toString();
     }
 }
