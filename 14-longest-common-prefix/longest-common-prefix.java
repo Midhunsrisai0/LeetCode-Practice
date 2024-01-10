c51b21235fa883499cf10ed06f7c6e75ccd76df5
Midhunsrisai0 <104824866+Midhunsrisai0@users.noreply.github.com>
2024-01-11 01:44:16 +0530

    Time: 11 ms (5.72%) | Memory: 42.2 MB (8.54%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,7 +1,7 @@
 import java.util.TreeSet;
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String fs="";
+        StringBuilder fs= new StringBuilder();
         int minLength=strs[0].length();
         for(String i: strs){
             int l;
@@ -12,9 +12,9 @@ class Solution {
             System.out.println("b --"+ i);
             TreeSet<Character> ts = new TreeSet<>();
             for(String s: strs) ts.add(s.charAt(i));
-            if(ts.size()==1) fs+=strs[0].charAt(i);
-            else return fs;
+            if(ts.size()==1) fs.append(strs[0].charAt(i));
+            else return fs.toString();
         }
-        return fs;
+        return fs.toString();
     }
 }
