9449076e2c0ea2ab372be6230effc25029148c91
Midhunsrisai0 <104824866+Midhunsrisai0@users.noreply.github.com>
2024-03-27 12:39:58 +0530

    Time: 314 ms (5.07%) | Memory: 47.2 MB (71.01%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -13,7 +13,8 @@ class Solution {
         return provinces;
     }
      void dfs(int n, boolean[] isVisited, int[][] mat){
-        for(int i=0;i<mat[n].length;i++){
+        for(int i=0;i<mat[0].length;i++){
+            System.out.println(n+"--"+mat[0].length);
             if(mat[n][i]==1 && !isVisited[i]){
                 isVisited[i]=true;
                 dfs(i,isVisited,mat);
